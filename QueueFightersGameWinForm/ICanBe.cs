655e113af900f02790b294efb6bd5a68e9de926b
Gorney-Alex <gorneyalexandr@gmail.com>
2025-04-07 22:16:46 +0300

    Mixed Interfaces

---
+++ QueueFightersGameWinForm/ICanBe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFightGame
+{
+    internal interface ICanBeHealed
+    {
+        string Name { get; }
+        float Health { get; set; }
+    }
+}
---
+++ QueueFightersGameWinForm/ISpecialActions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFightGame
+{
+    public interface ISpecialActionArcher
+    {
+        int Range { get; }
+        int Power { get; }
+        void DoSpecialAttack(IUnit target, Team ownTeam);
+    }
+
+    internal interface ISpecialActionHealer
+    {
+        int Range { get; }
+        int Power { get; }
+
+        void DoHeal(Team ownTeam);
+    }
+}
